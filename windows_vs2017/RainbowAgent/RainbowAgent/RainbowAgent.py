9c405c64881597fdab15f81f1a7dc526c4857562
decoderkurt <callmekoo@naver.com>
2018-04-25 23:21:07 +0900

    rainbow agent , custom_sonic_util

--- windows_vs2017/RainbowAgent/RainbowAgent/RainbowAgent.py
+++ windows_vs2017/RainbowAgent/RainbowAgent/RainbowAgent.py
@@ -15,45 +15,7 @@ from anyrl.rollouts import BatchedPlayer, PrioritizedReplayBuffer, NStepPlayer
 from anyrl.spaces import gym_space_vectorizer
 import gym_remote.exceptions as gre
 
-from sonic_util import AllowBacktracking, make_env
-
-def make_env(stack=True, scale_rew=True):
-    """
-    Create an environment with some standard wrappers.
-    """
-    env = grc.RemoteEnv('tmp/sock')
-    env = CustomSonicDiscretizer(env)
-    if scale_rew:
-        env = RewardScaler(env)
-    env = WarpFrame(env)
-    if stack:
-        env = FrameStack(env, 4)
-    return env
-
-class CustomSonicDiscretizer(SonicDiscretizer):
-    """
-    Wrap a gym-retro environment and make it use discrete
-    actions for the Sonic game.
-    """
-    def __init__(self, env):
-        super(SonicDiscretizer, self).__init__(env)
-        buttons = ["B", "A", "MODE", "START", "UP", "DOWN", "LEFT", "RIGHT", "C", "Y", "X", "Z"]
-        ##actions = [['LEFT'], ['RIGHT'], ['LEFT', 'DOWN'], ['RIGHT', 'DOWN'], ['DOWN'],
-        ##           ['DOWN', 'B'], ['B']]
-
-        actions = [['RIGHT'], ['RIGHT', 'B'], ['B']]
-
-        self._actions = []
-        for action in actions:
-            arr = np.array([False] * 12)
-            for button in action:
-                arr[buttons.index(button)] = True
-            self._actions.append(arr)
-        self.action_space = gym.spaces.Discrete(len(self._actions))
-
-    def action(self, a): # pylint: disable=W0221
-        return self._actions[a].copy()
-
+from custom_sonic_util import AllowBacktracking, make_env
 
 class RainbowPlayer(NStepPlayer):
     def _next_transition(self, history):
