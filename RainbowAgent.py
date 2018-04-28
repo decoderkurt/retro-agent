961e1c87889ccc3de258f7fe7cadfc894ceae13e
decoderkurt <callmekoo@naver.com>
2018-04-28 18:07:35 +0900

    file added

---
+++ RainbowAgent.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+
+"""
+Train an agent on Sonic using an open source Rainbow DQN
+implementation.
+"""
+
+import tensorflow as tf
+
+from anyrl.algos import DQN
+from anyrl.envs import BatchedGymEnv
+from anyrl.envs.wrappers import BatchedFrameStack
+from anyrl.models import rainbow_models
+from anyrl.rollouts import BatchedPlayer, PrioritizedReplayBuffer, NStepPlayer
+from anyrl.spaces import gym_space_vectorizer
+import gym_remote.exceptions as gre
+import time
+from custom_sonic_util import AllowBacktracking, make_env
+
+class RainbowPlayer(NStepPlayer):
+    def _play_once(self):
+        for trans in self.player.play():
+            assert len(trans['rewards']) == 1
+            ep_id = trans['episode_id']
+            if ep_id in self._ep_to_history:
+                self._ep_to_history[ep_id].append(trans)
+            else:
+                self._ep_to_history[ep_id] = [trans]
+        res = []
+        for ep_id, history in list(self._ep_to_history.items()):
+            while history:
+               # print(ep_id, ': history ' ,history)
+                trans = self._next_transition(history)
+                #print(ep_id, ': trans info' ,trans['info'])
+                if trans is None:
+                    break
+                res.append(trans)
+            if not history:
+                del self._ep_to_history[ep_id]
+        return res
+    def _next_transition(self, history):
+        if len(history) < self.num_steps:
+            if not history[-1]['is_last']:
+                return None
+        res = history[0].copy()
+        res['rewards'] = [h['rewards'][0] for h in history[:self.num_steps]]
+        res['total_reward'] += sum(h['rewards'][0] for h in history[1:self.num_steps])
+        if len(history) >= self.num_steps:
+            res['new_obs'] = history[self.num_steps-1]['new_obs']
+        else:
+            res['new_obs'] = None
+        del history[0]
+        return res
+
+class RainbowDQN(DQN):
+     # pylint: disable=R0913,R0914
+    def train(self,
+              num_steps,
+              player,
+              replay_buffer,
+              optimize_op,
+              train_interval=1,
+              target_interval=8192,
+              batch_size=32,
+              min_buffer_size=20000,
+              tf_schedules=(),
+              handle_ep=lambda steps, rew: None,
+              timeout=None):
+        sess = self.online_net.session
+        sess.run(self.update_target)
+        steps_taken = 0
+        next_target_update = target_interval
+        next_train_step = train_interval
+        start_time = time.time()
+        while steps_taken < num_steps:
+            if timeout is not None and time.time() - start_time > timeout:
+                return
+            transitions = player.play()
+            for trans in transitions:
+                if trans['is_last']:
+                    handle_ep(trans['episode_step'] + 1, trans['total_reward'])
+                replay_buffer.add_sample(trans)
+                steps_taken += 1
+                for sched in tf_schedules:
+                    sched.add_time(sess, 1)
+                if replay_buffer.size >= min_buffer_size and steps_taken >= next_train_step:
+                    next_train_step = steps_taken + train_interval
+                    batch = replay_buffer.sample(batch_size)
+                    _, losses = sess.run((optimize_op, self.losses),
+                                         feed_dict=self.feed_dict(batch))
+                    replay_buffer.update_weights(batch, losses)
+                if steps_taken >= next_target_update:
+                    next_target_update = steps_taken + target_interval
+                    sess.run(self.update_target)
+
+def main():
+    """Run DQN until the environment throws an exception."""
+    env = AllowBacktracking(make_env(stack=False, scale_rew=False))
+    env = BatchedFrameStack(BatchedGymEnv([[env]]), num_images=4, concat=False)
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True # pylint: disable=E1101
+    with tf.Session(config=config) as sess:
+        dqn = RainbowDQN(*rainbow_models(sess,
+                                  env.action_space.n,
+                                  gym_space_vectorizer(env.observation_space),
+                                  min_val=-200,
+                                  max_val=200))
+        player = RainbowPlayer(BatchedPlayer(env, dqn.online_net), 3)
+        optimize = dqn.optimize(learning_rate=1e-4)
+        sess.run(tf.global_variables_initializer())
+        dqn.train(num_steps=2000000, # Make sure an exception arrives before we stop.
+                  player=player,
+                  replay_buffer=PrioritizedReplayBuffer(500000, 0.5, 0.4, epsilon=0.1),
+                  optimize_op=optimize,
+                  train_interval=1,
+                  target_interval=8192,
+                  batch_size=32,
+                  min_buffer_size=20000)
+
+if __name__ == '__main__':
+    try:
+        main()
+    except gre.GymRemoteError as exc:
+        print('exception', exc)
