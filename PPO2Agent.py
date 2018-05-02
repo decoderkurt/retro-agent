b80bab773c2931e7c03ea1e013f144bad81ecdae
decoderkurt <callmekoo@naver.com>
2018-05-02 23:40:12 +0900

    make ppo2 executable

--- PPO2Agent.py
+++ PPO2Agent.py
@@ -7,13 +7,44 @@ Train an agent on Sonic using PPO2 from OpenAI Baselines.
 import tensorflow as tf
 
 from baselines.common.vec_env.dummy_vec_env import DummyVecEnv
+from baselines.common.vec_env import VecEnv
 import baselines.ppo2.ppo2 as ppo2
 import baselines.ppo2.policies as policies
 import gym_remote.exceptions as gre
-
+import numpy as np
+from gym import spaces
+from collections import OrderedDict
 from custom_sonic_util import make_env
 
 class PPO2DummyVecEnv(DummyVecEnv):
+    def __init__(self, env_fns):
+        self.envs = [fn() for fn in env_fns]
+        env = self.envs[0]
+        VecEnv.__init__(self, len(env_fns), env.observation_space, env.action_space)
+        shapes, dtypes = {}, {}
+        self.keys = []
+        obs_space = env.observation_space
+        if isinstance(obs_space, spaces.Dict):
+            assert isinstance(obs_space.spaces, OrderedDict)
+            for key, box in obs_space.spaces.items():
+                assert isinstance(box, spaces.Box)
+                shapes[key] = box.shape
+                dtypes[key] = box.dtype
+                self.keys.append(key)
+        else:
+            box = obs_space
+            assert isinstance(box, spaces.Box)
+            self.keys = [None]
+            shapes, dtypes = { None: box.shape }, { None: box.dtype }
+        self.buf_obs = { k: np.zeros((self.num_envs,) + tuple(shapes[k]), dtype=dtypes[k]) for k in self.keys }
+        self.buf_dones = np.zeros((self.num_envs,), dtype=np.bool)
+        self.buf_rews  = np.zeros((self.num_envs,), dtype=np.float32)
+        self.buf_infos = [{} for _ in range(self.num_envs)]
+        self.actions = None
+
+    def step_async(self, actions):
+        self.actions = actions
+
     def step_wait(self):
         for e in range(self.num_envs):
             obs, self.buf_rews[e], self.buf_dones[e], self.buf_infos[e] = self.envs[e].step(self.actions[e])
@@ -28,6 +59,22 @@ class PPO2DummyVecEnv(DummyVecEnv):
             obs = self.envs[e].reset()
             self._save_obs(e, obs)
         return self._obs_from_buf()
+
+    def close(self):
+        return
+
+    def _save_obs(self, e, obs):
+        for k in self.keys:
+            if k is None:
+                self.buf_obs[k][e] = obs
+            else:
+                self.buf_obs[k][e] = obs[k]
+
+    def _obs_from_buf(self):
+        if self.keys==[None]:
+            return self.buf_obs[None]
+        else:
+            return self.buf_obs
 
 def main():
     """Run PPO until the environment throws an exception."""
