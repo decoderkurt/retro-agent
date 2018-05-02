996f22fd07ba2ab7d6801ad3af0dc9463a25886c
decoderkurt <callmekoo@naver.com>
2018-05-02 23:16:48 +0900

    PPO2

---
+++ PPO2Agent.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+"""
+Train an agent on Sonic using PPO2 from OpenAI Baselines.
+"""
+
+import tensorflow as tf
+
+from baselines.common.vec_env.dummy_vec_env import DummyVecEnv
+import baselines.ppo2.ppo2 as ppo2
+import baselines.ppo2.policies as policies
+import gym_remote.exceptions as gre
+
+from custom_sonic_util import make_env
+
+class PPO2DummyVecEnv(DummyVecEnv):
+    def step_wait(self):
+        for e in range(self.num_envs):
+            obs, self.buf_rews[e], self.buf_dones[e], self.buf_infos[e] = self.envs[e].step(self.actions[e])
+            if self.buf_dones[e]:
+                obs = self.envs[e].reset()
+            self._save_obs(e, obs)
+        return (self._obs_from_buf(), np.copy(self.buf_rews), np.copy(self.buf_dones),
+                self.buf_infos.copy())
+
+    def reset(self):
+        for e in range(self.num_envs):
+            obs = self.envs[e].reset()
+            self._save_obs(e, obs)
+        return self._obs_from_buf()
+
+def main():
+    """Run PPO until the environment throws an exception."""
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True # pylint: disable=E1101
+    with tf.Session(config=config):
+        # Take more timesteps than we need to be sure that
+        # we stop due to an exception.
+        ppo2.learn(policy=policies.CnnPolicy,
+                   env=PPO2DummyVecEnv([make_env]),
+                   nsteps=4096,
+                   nminibatches=8,
+                   lam=0.95,
+                   gamma=0.99,
+                   noptepochs=3,
+                   log_interval=1,
+                   ent_coef=0.01,
+                   lr=lambda _: 2e-4,
+                   cliprange=lambda _: 0.1,
+                   total_timesteps=int(1e7))
+
+if __name__ == '__main__':
+    try:
+        main()
+    except gre.GymRemoteError as exc:
+        print('exception', exc)
+
