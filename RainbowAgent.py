c0094332765e92e0e24b22b3abe91c317ee2f6b4
decoderkurt <callmekoo@naver.com>
2018-05-07 00:05:07 +0900

    discount factor

--- RainbowAgent.py
+++ RainbowAgent.py
@@ -94,7 +94,7 @@ class RainbowPlayer(NStepPlayer):
         return res
 
 class RainbowDQN(DQN):
-    def __init__(self, online_net, target_net, discount=0.85):
+    def __init__(self, online_net, target_net, discount=0.90):
 
         self.online_net = online_net
         self.target_net = target_net
