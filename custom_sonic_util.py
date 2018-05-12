0396713b25162e4786ac99a042d0d5b8e0cd6953
decoderkurt <callmekoo@naver.com>
2018-05-12 18:11:48 +0900

    delete unused code

--- custom_sonic_util.py
+++ custom_sonic_util.py
@@ -23,28 +23,7 @@ def make_env(stack=True, scale_rew=True):
         env = FrameStack(env, 4)
     return env
 
-class SonicDiscretizer(gym.ActionWrapper):
-    """
-    Wrap a gym-retro environment and make it use discrete
-    actions for the Sonic game.
-    """
-    def __init__(self, env):
-        super(SonicDiscretizer, self).__init__(env)
-        buttons = ["B", "A", "MODE", "START", "UP", "DOWN", "LEFT", "RIGHT", "C", "Y", "X", "Z"]
-        actions = [['LEFT'], ['RIGHT'], ['LEFT', 'DOWN'], ['RIGHT', 'DOWN'], ['DOWN'],
-                   ['DOWN', 'B'], ['B']]
-        self._actions = []
-        for action in actions:
-            arr = np.array([False] * 12)
-            for button in action:
-                arr[buttons.index(button)] = True
-            self._actions.append(arr)
-        self.action_space = gym.spaces.Discrete(len(self._actions))
-
-    def action(self, a): # pylint: disable=W0221
-        return self._actions[a].copy()
-
-class CustomSonicDiscretizer(SonicDiscretizer):
+class CustomSonicDiscretizer(gym.ActionWrapper):
     """
     Wrap a gym-retro environment and make it use discrete
     actions for the Sonic game.
