4df6651a7c3370d3c6221e8a334eea319fdf2978
kurt <callmekoo@naver.com>
2018-05-14 14:20:11 +0900

    alpha 0.7, step 5

--- RainbowAgent.py
+++ RainbowAgent.py
@@ -182,12 +182,12 @@ def main():
                                   gym_space_vectorizer(env.observation_space),
                                   min_val=-200,
                                   max_val=200))
-        player = RainbowPlayer(RainbowBatchedPlayer(env, dqn.online_net), 6)
+        player = RainbowPlayer(RainbowBatchedPlayer(env, dqn.online_net), 5)
         optimize = dqn.optimize(learning_rate=1e-4)
         sess.run(tf.global_variables_initializer())
         dqn.train(num_steps=2000000, # Make sure an exception arrives before we stop.
                   player=player,
-                  replay_buffer=PrioritizedReplayBuffer(500000, 0.5, 0.4, epsilon=0.1),
+                  replay_buffer=PrioritizedReplayBuffer(500000, 0.7, 0.4, epsilon=0.1),
                   optimize_op=optimize,
                   train_interval=1,
                   target_interval=8192,
